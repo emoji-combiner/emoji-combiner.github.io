6078e0032bdac56e0efaaa7ebc06579316c63690
Geza Kovacs <gkovacs@stanford.edu>
2016-10-27 11:11:24 -0700

    fix decomposer

--- index.js
+++ index.js
@@ -67,13 +67,15 @@
 	    if (next_char == null) {
 	      break;
 	    }
-	    if (next_char === 0x200D || next_char === 0xdc66 || next_char === 0xdc67 || next_char === 0xdc68 || next_char === 0xdc69 || next_char === 0xdc8b) {
+	    if (next_char === 0x200D || next_char === 0xdc66 || next_char === 0xdc67 || next_char === 0xdc68 || next_char === 0xdc69 || next_char === 0xdc8b || next_char === 0xdc41 || next_char === 0xdde8) {
 	      continue;
 	    }
 	    as_string = String.fromCodePoint(next_char);
 	    if (as_string.codePointAt(0) === 0xfffd) {
 	      continue;
 	    }
+	    console.log(as_string);
+	    console.log(as_string.codePointAt(0));
 	    output.push(as_string);
 	  }
 	  return $('#span_display2').text(output.join(''));
--- index.ls
+++ index.ls
@@ -18,11 +18,13 @@ window.text_update2 = text_update2 = ->
     position += 1
     if not next_char?
       break
-    if next_char == 0x200D or next_char == 0xdc66 or next_char == 0xdc67 or next_char == 0xdc68 or next_char == 0xdc69 or next_char == 0xdc8b
+    if next_char == 0x200D or next_char == 0xdc66 or next_char == 0xdc67 or next_char == 0xdc68 or next_char == 0xdc69 or next_char == 0xdc8b or next_char == 0xdc41 or next_char == 0xdde8
       continue
     as_string = String.fromCodePoint(next_char)
     if as_string.codePointAt(0) == 0xfffd
       continue
+    console.log as_string
+    console.log as_string.codePointAt(0)
     output.push as_string
   $('#span_display2').text output.join('')
 
